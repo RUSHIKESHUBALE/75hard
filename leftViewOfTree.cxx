eec68b4b7f2187b7ec76bf3dca82b5a8f2316bed
Rushikesh Ubale <46742901+RUSHIKESHUBALE@users.noreply.github.com>
2023-10-11 17:34:19 +0530

    Day 10: Left view of Tree

---
+++ leftViewOfTree.cxx
@@ -0,0 +1,55 @@
+// for given binary tree print all the nodes if seen from right side of tree
+//    1
+//   / \
+//  2   3
+// Here nodes visible from right are 1 and 3
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node{
+    node* right;
+    node* left;
+    int data;
+
+    node( int val )
+    {
+        right = NULL;
+        left = NULL;
+        data = val;
+    }
+};
+
+void getLeftView( node* root )
+{
+   queue<node*> q;
+   q.push( root );
+
+   while( !q.empty() )
+   {
+        int n = q.size();
+        for ( int i = 0; i < n; i++ )
+        {
+            if ( i == 0 )
+            {
+                cout<<q.front()->data<<" ";
+            }
+            if( q.front()->left ) q.push( q.front()->left );
+            if( q.front()->right ) q.push( q.front()->right );
+            q.pop();
+        }
+   } 
+}
+
+int main()
+{
+    node* root = new node( 1 );
+    root->left = new node( 2 );
+    root->right = new node( 3 );
+    root->right->left = new node( 5 );
+    root->right->left->left = new node( 7 );
+    root->right->right = new node( 6 );
+    root->left->left = new node( 4 );
+
+    getLeftView( root );
+}
